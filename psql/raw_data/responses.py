dbe3c4748bd2a54ecedb8b1f8920fda1b8d2e87c
Azaria Yonas <azariayonas8@gmail.com>
2026-03-14 17:59:03 -0700

    Added Method to Fetch Responses

--- psql/raw_data/responses.py
+++ psql/raw_data/responses.py
@@ -1,13 +1,55 @@
-import psycopg
+import psycopg as pg
 from psycopg.types.json import Json
 
-from .. import DATABASE, USERNAME, DB_KEY
+from .. import DATABASE, USERNAME, DB_KEY, CITY
 
 def insert_response(zcta, city, api, response):
-    with psycopg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
+    with pg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
         with conn.cursor() as curr:
             curr.execute("""
                 INSERT INTO raw_data.responses (zcta, city, api, response) 
                 VALUES (%s, %s, %s, %s);
             """, 
             (zcta, city, api, Json(response)))
+
+
+
+def get_response(api, lbound=None, hbound=None):
+
+    responses = []
+
+    with pg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
+        with conn.cursor() as curr:
+            if lbound is None and hbound is None:
+
+                curr.execute("""
+                    SELECT response
+                    FROM raw_data.responses
+                    WHERE city = %s AND api = %s
+                """, (CITY, api))
+            elif lbound is not None and hbound is None:
+                curr.execute("""
+                    SELECT response
+                    FROM raw_data.responses
+                    WHERE city = %s AND api = %s
+                    OFFSET %s
+                """, (CITY, api, lbound))
+            elif lbound is None and hbound is not None:
+                curr.execute("""
+                    SELECT response
+                    FROM raw_data.responses
+                    WHERE city = %s AND api = %s
+                    FETCH FIRST %s ROWS ONLY
+                """, (CITY, api, hbound))
+            else:
+                curr.execute("""
+                    SELECT response
+                    FROM raw_data.responses
+                    WHERE city = %s AND api = %s
+                    OFFSET %s
+                    FETCH FIRST %s ROWS ONLY
+                """, (CITY, api, lbound, hbound - lbound if hbound and lbound is not None else 0))
+            for (r,) in curr:
+                responses.append(r)
+
+    return responses
