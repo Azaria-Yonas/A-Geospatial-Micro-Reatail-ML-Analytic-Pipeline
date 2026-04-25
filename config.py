33ab409a969b5905921628a02f66e851d685ad18
Azaria Yonas <azariayonas@azarias-mbp-2.biola.edu>
2026-04-24 19:16:00 -0700

    Added Config File

---
+++ config.py
@@ -0,0 +1,16 @@
+
+import os
+
+# Database Connection
+
+DATABASE = "ninthrun"  
+DB_KEY = os.getenv("dbKey")
+USERNAME = os.getenv("psqlUser")
+CITY = "seattle"
+
+#API Keys
+
+API_KEY = os.getenv("ArcGIS")
+API_KEY = os.getenv("Places_Aggregate")
+
+
