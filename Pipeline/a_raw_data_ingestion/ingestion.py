ce118fe22a742078cbed6829d8d7a6a7e96b3cc9
Azaria Yonas <azariayonas8@email.com>
2026-08-02 14:17:39 -0700

    renamed files

--- Pipeline/a_raw_data_ingestion/ingestion.py
+++ Pipeline/a_raw_data_ingestion/ingestion.py
@@ -1,12 +1,12 @@
 import asyncio
 import aiohttp
-
 from psql.raw_data.coordinates import get_coordinates
 from a_raw_data_ingestion.places_request import places_tasks
 from a_raw_data_ingestion.overpass_request import overpass_tasks
-from a_raw_data_ingestion.census_request import census_tasks
-from a_raw_data_ingestion.arcgis_request import arcgis_tasks
-from a_raw_data_ingestion.cbp_request import cbp_tasks
+from Pipeline.a_raw_data_ingestion.acs import acs_tasks
+from Pipeline.a_raw_data_ingestion.arcgis import arcgis_tasks
+from Pipeline.a_raw_data_ingestion.cbp_naics import cbp_tasks
+from Pipeline.a_raw_data_ingestion.dhc import dhc_tasks  
 from psql.raw_data.responses import insert_response
 
 
@@ -25,11 +25,12 @@ async def ingest_data():
     async with aiohttp.ClientSession() as session:
         # places = [places_tasks(session, coordinate) for  coordinate in coordinates] 
         # overpass = [overpass_tasks(session, coordinate) for  coordinate in coordinates] 
-        census = [census_tasks(session, coordinate[0]) for  coordinate in coordinates] 
+        acs = [acs_tasks(session, coordinate[0]) for  coordinate in coordinates] 
         arcgis = [arcgis_tasks(session, coordinate[0]) for  coordinate in coordinates]
         cbp =  [cbp_tasks(session, coordinate[0])  for  coordinate in coordinates]
-
-        results = await asyncio.gather(*census, *arcgis, *cbp, return_exceptions=True)
+        dhc = [dhc_tasks(session, coordinate[0]) for coordinate in coordinates]  
+        
+        results = await asyncio.gather(*acs, *arcgis, *cbp, *dhc, return_exceptions=True)
 
         for result in results:
             if not isinstance(result, tuple) or len(result) != 4:
@@ -40,6 +41,7 @@ async def ingest_data():
 
             if s in (200,204,206):
                 insert_response(z, city, n, r)
+                
 
 
 
