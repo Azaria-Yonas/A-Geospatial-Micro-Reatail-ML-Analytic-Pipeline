1eafd42b5b501035fa64861b771b7ce3576c54ee
Azaria Yonas <azariayonas8@email.com>
2026-08-02 14:09:36 -0700

    a dded key for census

--- Pipeline/config.py
+++ Pipeline/config.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env/.env") 
+
 
 # Postgres Credentials:
 PSQL_KEY = os.getenv("DB_KEY")
-PSQL_USER = os.getenv("USERNAME")
+PSQL_USER = os.getenv("USERNAME")          
+
 
 # API Keys:
 PLACES = os.getenv("PLACES_KEY")
 ARGIS = os.getenv("ARGIS_KEY")  
+CENSUS = os.getenv("CENSUS_KEY") 
+
+
+
+
 
 
