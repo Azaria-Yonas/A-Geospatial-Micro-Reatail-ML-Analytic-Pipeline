bc29b88b447b3a5844d0d8a930b6fe3fd6eac27f
Azaria Yonas <azariayonas8@email.com>
2026-08-03 19:47:04 -0700

    City --> State

--- Pipeline/psql/raw_data/locations.py
+++ Pipeline/psql/raw_data/locations.py
@@ -2,18 +2,19 @@ import psycopg
 from .. import DATABASE, USERNAME, DB_KEY
 
 
-def insert_location (zcta, city, bbox):
+def insert_location (zcta, state, bbox): 
+    """This function serves to insert the coordinate representations of each ZCTA into the table locations"""
     with psycopg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
         with conn.cursor() as cur:
             if bbox is (None): 
                 cur.execute("""
-                    INSERT INTO raw_data.locations(zcta, city, down_lat, left_long, up_lat, right_long)
+                    INSERT INTO raw_data.locations(zcta, state, down_lat, left_long, up_lat, right_long)
                     VALUES (%s, %s, %s, %s, %s, %s);
                 """,
-                (zcta, city, 0 , 0 , 0 , 0))
+                (zcta, state, 0 , 0 , 0 , 0))
             else:
                 cur.execute("""
-                    INSERT INTO raw_data.locations(zcta, city, down_lat, left_long, up_lat, right_long)
+                    INSERT INTO raw_data.locations(zcta, state, down_lat, left_long, up_lat, right_long)
                     VALUES (%s, %s, %s, %s, %s, %s);
                 """,
-                (zcta, city, bbox[0], bbox[1], bbox[2], bbox[3]))  
+                (zcta, state, bbox[0], bbox[1], bbox[2], bbox[3]))  
