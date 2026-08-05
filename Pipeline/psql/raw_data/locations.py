c97caa5fe0729852a061c2e2b6309de1d5abb86e
Azaria Yonas <azariayonas8@email.com>
2026-08-05 14:05:44 -0700

    Added new queries for fetching error requests and updating them

--- Pipeline/psql/raw_data/locations.py
+++ Pipeline/psql/raw_data/locations.py
@@ -18,3 +18,45 @@ def insert_location (zcta, state, bbox):
                     VALUES (%s, %s, %s, %s, %s, %s);
                 """,
                 (zcta, state, bbox[0], bbox[1], bbox[2], bbox[3]))  
+
+
+
+
+def get_errors():
+    """This function extracts insertions of bad requests for retries."""
+
+    results = [] 
+
+    with psycopg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn: 
+        with conn.cursor() as curr: 
+            curr.execute("""
+                SELECT zcta, state FROM raw_data.locations WHERE down_lat = 0 OR up_lat = 0 OR left_long = 0 OR right_long = 0
+            """)
+
+            results = curr.fetchall()
+
+    return results 
+
+
+
+
+def update_zcta(zcta, state, bbox):
+    """This function updates the error requests with the latest retires."""
+    if bbox is None: 
+        return 
+
+    with psycopg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn: 
+        with conn.cursor() as curr:
+            curr.execute("""
+                UPDATE raw_data.locations SET down_lat = %s, left_long = %s, up_lat = %s, right_long = %s
+                WHERE zcta = %s AND state = %s
+                """,
+                (zcta, state, bbox[0], bbox[1], bbox[2], bbox[3])) 
+
+
+
+
+
+
+
+
