ca4f76946a597dcc4dabdd7c2afeceef7c4b20e7
Azaria Yonas <azariayonas8@email.com>
2026-08-05 16:52:11 -0700

    Changed Column: zcta INT --> zcta VARCHAR(5)

--- Pipeline/a_raw_data_ingestion/SQL/responses.sql
+++ Pipeline/a_raw_data_ingestion/SQL/responses.sql
@@ -2,10 +2,10 @@ CREATE TABLE raw_data.responses(
     index_num BIGSERIAL NOT NULL,
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    zcta INT REFERENCES raw_data.zcta(zcta),
-    city VARCHAR(30),
+    zcta VARCHAR(5) REFERENCES raw_data.zcta(zcta),
+    state VARCHAR(30),
 
-    api VARCHAR(8) NOT NULL CHECK (api IN ('places','overpass','arcgis','census', 'cbp')),
+    api VARCHAR(8) NOT NULL,
     response JSONB,
     date_time TIMESTAMPTZ DEFAULT now()
 );
@@ -13,4 +13,4 @@ CREATE TABLE raw_data.responses(
 
 CREATE INDEX responses_zcta_and_api ON raw_data.responses(zcta, api);
 CREATE INDEX responses_date_time ON raw_data.responses(date_time);
-CREATE INDEX responses_city_idx ON raw_data.responses(city); 
+CREATE INDEX responses_state_idx ON raw_data.responses(state); 
