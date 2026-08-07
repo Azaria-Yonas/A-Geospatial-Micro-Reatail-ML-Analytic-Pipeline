0826c3968d6554255cb78b6e6dadf3034f627e4c
Azaria Yonas <azariayonas8@email.com>
2026-08-06 17:11:59 -0700

    Building Queries Dynamically and fixed minor incompatibility errors with dhc and cbp_naics requests

--- Pipeline/psql/raw_data/responses.py
+++ Pipeline/psql/raw_data/responses.py
@@ -1,54 +1,47 @@
 import psycopg as pg
 from psycopg.types.json import Json
 
-from .. import DATABASE, USERNAME, DB_KEY, CITY
+from .. import DATABASE, USERNAME, DB_KEY 
 
-def insert_response(zcta, city, api, response):
+
+def insert_response(zcta, state, api, response):
     with pg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
         with conn.cursor() as curr:
             curr.execute("""
-                INSERT INTO raw_data.responses (zcta, city, api, response) 
+                INSERT INTO raw_data.responses (zcta, state, api, response) 
                 VALUES (%s, %s, %s, %s);
             """, 
-            (zcta, city, api, Json(response)))
+            (zcta, state, api, Json(response)))
 
 
 
-def get_response(api, city = CITY, lbound=None, hbound=None):
+def get_response(api, state = None, lbound=None, hbound=None):
 
     responses = {}
 
     with pg.connect(f"dbname={DATABASE} user={USERNAME} password={DB_KEY}") as conn:
         with conn.cursor() as curr:
-            if lbound is None and hbound is None:
 
-                curr.execute("""
-                    SELECT zcta ,response
-                    FROM raw_data.responses
-                    WHERE city = %s AND api = %s
-                """, (city, api))
-            elif lbound is not None and hbound is None:
-                curr.execute("""
-                    SELECT zcta, response
-                    FROM raw_data.responses
-                    WHERE city = %s AND api = %s
-                    OFFSET %s
-                """, (city, api, lbound))
-            elif lbound is None and hbound is not None:
-                curr.execute("""
-                    SELECT zcta, response
-                    FROM raw_data.responses
-                    WHERE city = %s AND api = %s
-                    FETCH FIRST %s ROWS ONLY
-                """, (city, api, hbound))
-            else:
-                curr.execute("""
-                    SELECT zcta, response
-                    FROM raw_data.responses
-                    WHERE city = %s AND api = %s
-                    OFFSET %s
-                    FETCH FIRST %s ROWS ONLY
-                """, (city, api, lbound, hbound - lbound if hbound and lbound is not None else 0))
+            parameters = [] 
+
+
+            query = "SELECT zcta, response FROM raw_data.responses"
+
+            if state is not None: 
+                query += " WHERE state = %s AND api = %s"
+                parameters.extend((state, api))
+
+
+            if lbound is not None:
+                query += " OFFSET %s"
+                parameters.append(lbound)
+
+            if hbound is not None:
+                query += " FETCH FIRST %s ROWS ONLY"
+                parameters.append(hbound)
+
+            curr.execute(query=query, params=parameters)
+
             for (zcta,r) in curr:
                 responses[zcta] = r
 
