be505ab0c0eba3a463784c14df815693f3d2ef27
Azaria Yonas <azariayonas8@email.com>
2026-08-07 11:38:16 -0700

    Added tables to match the new requests and variables

---
+++ Pipeline/b_data_parsing/SQL/acs.sql
@@ -0,0 +1,45 @@
+CREATE TABLE processed_data.acs_variables (
+    index_num BIGSERIAL NOT NULL,
+    zcta VARCHAR(5) PRIMARY KEY REFERENCES raw_data.zcta(zcta),
+    state VARCHAR(3),
+
+
+    total_population INTEGER,
+    median_household_income INTEGER, 
+    per_capita_income INTEGER, 
+    poverty_population INTEGER,
+    bachelors_degree INTEGER,
+    employed_population INTEGER,
+    housing_units INTEGER,  
+    median_gross_rent INTEGER,
+    public_transport_users INTEGER,
+    total_commuters INTEGER,
+    aggregate_commute_time INTEGER,
+    no_vehicle_households INTEGER,
+    renter_occupied INTEGER,
+    owner_occupied INTEGER,
+    total_occupied INTEGER,
+    food_stamp_households INTEGER,
+    total_households INTEGER,
+    owner_no_vehicle_households INTEGER
+
+);
+
+
+
+CREATE INDEX acs_state_zcta ON processed_data.acs_variables (state, zcta); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
